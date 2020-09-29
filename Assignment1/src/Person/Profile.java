bb999cb5ef95334f67c109ae28fcee9e5106de7c
JiaqiWang <wang.jiaqi6@northeastern.edu>
2020-09-29 12:11:16 -0400

    Assignment1 Commitment

---
+++ Assignment1/src/Person/Profile.java
@@ -0,0 +1,187 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Person;
+
+import javax.swing.ImageIcon;
+
+/**
+ *
+ * @author User
+ */
+public class Profile {
+    private String Name;
+     private String Geographic_data;
+     private String Date_of_birth;
+      private String Telephone_num;
+    private  String FAX_num;
+     private String Email_address;
+     private String SocialSecurity_num;
+     private String Medicalrecord_num;
+      private String Health_plan_beneficiary_num;
+      private String Bankaccount_num;        
+     private String Certificate_license_num;
+     private String  Vehicle_id;
+      private String Device_id;
+      private String LinkedIn;      
+      private String  Internet_protocol_addresses;      
+       private String  Any_unique_identifying_number_characteristic_or_code; 
+       private String Biometric_identifiers;
+       private String photopath;
+
+    public String getPhotopath() {
+        return photopath;
+    }
+
+    public void setPhotopath(String photopath) {
+        this.photopath = photopath;
+    }
+
+   
+
+ 
+    
+
+    public String getBiometric_identifiers() {
+        return Biometric_identifiers;
+    }
+
+    public void setBiometric_identifiers(String Biometric_identifiers) {
+        this.Biometric_identifiers = Biometric_identifiers;
+    }
+
+    public String getAny_unique_identifying_number_characteristic_or_code() {
+        return Any_unique_identifying_number_characteristic_or_code;
+    }
+
+    public void setAny_unique_identifying_number_characteristic_or_code(String Any_unique_identifying_number_characteristic_or_code) {
+        this.Any_unique_identifying_number_characteristic_or_code = Any_unique_identifying_number_characteristic_or_code;
+    }
+
+   
+
+    public String getName() {
+        return Name;
+    }
+
+    public void setName(String Name) {
+        this.Name = Name;
+    }
+
+    public String getGeographic_data() {
+        return Geographic_data;
+    }
+
+    public void setGeographic_data(String Geographic_data) {
+        this.Geographic_data = Geographic_data;
+    }
+
+    public String getDate_of_birth() {
+        return Date_of_birth;
+    }
+
+    public void setDate_of_birth(String Date_of_birth) {
+        this.Date_of_birth = Date_of_birth;
+    }
+
+    public String getTelephone_num() {
+        return Telephone_num;
+    }
+
+    public void setTelephone_num(String Telephone_num) {
+        this.Telephone_num = Telephone_num;
+    }
+
+    public String getFAX_num() {
+        return FAX_num;
+    }
+
+    public void setFAX_num(String FAX_num) {
+        this.FAX_num = FAX_num;
+    }
+
+    public String getEmail_address() {
+        return Email_address;
+    }
+
+    public void setEmail_address(String Email_address) {
+        this.Email_address = Email_address;
+    }
+
+    public String getSocialSecurity_num() {
+        return SocialSecurity_num;
+    }
+
+    public void setSocialSecurity_num(String SocialSecurity_num) {
+        this.SocialSecurity_num = SocialSecurity_num;
+    }
+
+    public String getMedicalrecord_num() {
+        return Medicalrecord_num;
+    }
+
+    public void setMedicalrecord_num(String Medicalrecord_num) {
+        this.Medicalrecord_num = Medicalrecord_num;
+    }
+
+    public String getHealth_plan_beneficiary_num() {
+        return Health_plan_beneficiary_num;
+    }
+
+    public void setHealth_plan_beneficiary_num(String Health_plan_beneficiary_num) {
+        this.Health_plan_beneficiary_num = Health_plan_beneficiary_num;
+    }
+
+    public String getBankaccount_num() {
+        return Bankaccount_num;
+    }
+
+    public void setBankaccount_num(String Bankaccount_num) {
+        this.Bankaccount_num = Bankaccount_num;
+    }
+
+    public String getCertificate_license_num() {
+        return Certificate_license_num;
+    }
+
+    public void setCertificate_license_num(String Certificate_license_num) {
+        this.Certificate_license_num = Certificate_license_num;
+    }
+
+    public String getVehicle_id() {
+        return Vehicle_id;
+    }
+
+    public void setVehicle_id(String Vehicle_id) {
+        this.Vehicle_id = Vehicle_id;
+    }
+
+    public String getDevice_id() {
+        return Device_id;
+    }
+
+    public void setDevice_id(String Device_id) {
+        this.Device_id = Device_id;
+    }
+
+    public String getLinkedIn() {
+        return LinkedIn;
+    }
+
+    public void setLinkedIn(String LinkedIn) {
+        this.LinkedIn = LinkedIn;
+    }
+
+    public String getInternet_protocol_addresses() {
+        return Internet_protocol_addresses;
+    }
+
+    public void setInternet_protocol_addresses(String Internet_protocol_addresses) {
+        this.Internet_protocol_addresses = Internet_protocol_addresses;
+    }
+
+
+   
+}
